6f95a4de7a6b32154b2a70ea5d936a0770cbe5f0
Thiago <thiagoximeneslima@gmail.com>
2019-05-13 00:35:07 -0300

    adicionando o exercócio 31 ainda não corrigido

---
+++ pythonexercicios/ex031.py
@@ -0,0 +1,7 @@
+distancia = float(input('Digite a distância da viagem em km: '))
+if distancia <= 200:
+    passagem = distancia * 0.5
+    print('Para {:.0f} km o valor da passagem é R$ {:.2f}'.format(distancia, passagem))
+else:
+    passagem = distancia * 0.45
+    print('Para {:.0f} km o valor da passagem é R$ {:.2f}'.format(distancia, passagem))
