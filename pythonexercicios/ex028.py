eb8df340e3e88c86984d10111c9dd9566cdd2e7c
Thiago <thiagoximeneslima@gmail.com>
2019-05-13 19:41:22 -0300

    Exercicios resolvidos

--- pythonexercicios/ex028.py
+++ pythonexercicios/ex028.py
@@ -1,9 +1,12 @@
 from random import randint
 import emoji
-n = randint(0,5) # o computador seleciona
+from time import sleep
+n = randint(0,5) # o computador seleciona um número entre 0 e 5
 print('-_-'*20)
-resp = int(input('TENTE ACERTAR O QUE O COMPUTADOR PENSOU!\n Digite um número entre 0 e 5: '))
+resp = int(input('TENTE ACERTAR O QUE O COMPUTADOR PENSOU!\n Digite um número entre 0 e 5: ')) # o usuário tenta acertar o número
+print('PROCESSANDO...')
 print('-_-'*20)
+sleep(2)
 if n == resp:
     print(emoji.emojize('TEMOS UM VENCEDOR!!! :gem:', use_aliases=True))
 else:
--- pythonexercicios/ex029.py
+++ pythonexercicios/ex029.py
@@ -2,3 +2,5 @@ carro = int(input('Digite a que velocidade está o carro: '))
 if carro > 80:
     multa = (carro - 80) * 7
     print('VOCÊ FOI MULTADO! O valor da MULTA é de R$ {:.2f}'.format(multa))
+else:
+    print('Sua velocidade está boa, PARABÉNS!')
