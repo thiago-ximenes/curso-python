a902cf8c7e585808cb6d743cb2b83bd0271cca81
Thiago <thiagoximeneslima@gmail.com>
2019-05-13 23:43:47 -0300

    Aula 9 atualizada

--- pythonteste/aula11a.py
+++ pythonteste/aula11a.py
@@ -1,1 +1,7 @@
 print('\n\n\033[7;37;40mHello World!\033[m\n\n')
+nome = 'Miguel'
+cores = {'limpa': '\033[m',
+    'azul':'\033[34m',
+    'amarelo':'\033[33',
+    'pretoebranco':'\033[30;47m'}
+print('Olá, você é muito gostoso de papai! {}{}, papai te ama!{}'.format(cores['pretoebranco'], nome, cores['limpa']))
