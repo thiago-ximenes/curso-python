10a00835acfd9ededcba906517521a9eb8e85148
Thiago <thiagoximeneslima@gmail.com>
2019-05-16 12:56:50 -0300

    cores no exercícios 2 e 3

--- pythonexercicios/ex003.py
+++ pythonexercicios/ex003.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
+n1 = int(input('\033[7mDigite um número:\033[m '))
+n2 = int(input('\033[7mDigite outro número:\033[m '))
 s = n1+n2
-print('A soma entre {} e {} é: {}.'.format(n1,n2,s))
+print('A soma entre \033[1;31;43m{}\033[m e \033[1;31;43m{}\033[m é: \033[1;31;43m{}\033[m.'.format(n1,n2,s))
