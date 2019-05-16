0770020cf9f8affee9bd52f62646c74b9607cd7b
Thiago <thiagoximeneslima@gmail.com>
2019-05-16 17:33:13 -0300

    Atualização de arquivos

--- pythonexercicios/ex028.py
+++ pythonexercicios/ex028.py
@@ -3,11 +3,11 @@ import emoji
 from time import sleep
 n = randint(0,5) # o computador seleciona um número entre 0 e 5
 print('-_-'*20)
-resp = int(input('TENTE ACERTAR O QUE O COMPUTADOR PENSOU!\n Digite um número entre 0 e 5: ')) # o usuário tenta acertar o número
+resp = int(input('\033[7mTENTE ACERTAR O QUE O COMPUTADOR PENSOU!\033[m\n \033[32mDigite um número entre 0 e 5: \033[m')) # o usuário tenta acertar o número
 print('PROCESSANDO...')
 print('-_-'*20)
 sleep(2)
 if n == resp:
-    print(emoji.emojize('TEMOS UM VENCEDOR!!! :gem:', use_aliases=True))
+    print(emoji.emojize('\033[7;47;30mTEMOS UM VENCEDOR!!!\033[m :gem:', use_aliases=True))
 else:
-    print(emoji.emojize('IHHHHH o computador pensou no {} :confounded: ! TENTE NOVAMENTE!'.format(n),use_aliases=True))
+    print(emoji.emojize('\033[41mIHHHHH o computador pensou no {} :confounded: ! TENTE NOVAMENTE!\033[m'.format(n),use_aliases=True))
--- pythonteste/aula06a.py
+++ pythonteste/aula06a.py
@@ -1,5 +1,5 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro: '))
+n1 = int(input('\033[30;47mDigite um valor:\033[m '))
+n2 = int(input('\033[30;47mDigite outro:\033[m '))
 s = n1 + n2 
 # print('A soma vale entre', n1, 'e', n2, 'vale', s)
-print('a soma entre {0} e {1} vale {2}'.format(n1, n2, s))
+print('a soma entre \033[4;33m{0}\033[m e \033[4;33m{1}\033[m vale \033[31;4m{2}\033[m'.format(n1, n2, s))
