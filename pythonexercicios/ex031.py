f29b3b3336592dee2b46f14ef0595f22dec57a88
Thiago <thiagoximeneslima@gmail.com>
2019-05-16 12:27:40 -0300

    colorindo e atualizando exercicios para pratica

--- pythonexercicios/ex031.py
+++ pythonexercicios/ex031.py
@@ -1,5 +1,5 @@
 distancia = float(input('Digite a distância da viagem em km: '))
-if distancia <= 200:
+'''if distancia <= 200:
     passagem = distancia * 0.5
     print('Para {:.0f} km o valor da passagem é R$ {:.2f}'.format(distancia, passagem))
 else:
@@ -7,4 +7,9 @@ else:
     print('Para {:.0f} km o valor da passagem é R$ {:.2f}'.format(distancia, passagem))
 
 preco = distancia * 0.5 if distancia <= 200 else distancia * 0.45
-print(preco)
+print(preco)'''
+if distancia <= 200:
+    passagem = distancia * 0.5
+else:
+    passagem = distancia * 0.45
+print('\033[4mPara a viagem de {}km será cobrado o valor de passagem de:\033[m \033[32;7mR$ {:.2f}\033[m'.format(distancia, passagem))
--- pythonteste/aula06b.py
+++ pythonteste/aula06b.py
@@ -1,4 +1,4 @@
-n=input('Digite um valor: ')
+n=input('\033[mDigite um valor: \033')
 print(n.is())
 print(n.isalpha())
 print(n.isupper())
