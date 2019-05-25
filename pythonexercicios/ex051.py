97e456930ba8c002b3b73761e5aef821975d09e8
Thiago <thiagoximeneslima@gmail.com>
2019-05-25 20:19:53 -0300

    ex051

--- pythonexercicios/ex051.py
+++ pythonexercicios/ex051.py
@@ -2,7 +2,7 @@ c = int(input('Digite o primeiro termo da P.A: '))
 r = int(input('Digite a razão da P.A: '))
 z = c
 print('Abaixo os 10 primeiros números da P.A. do primeiro ter {} e razão {}: '.format(c, r))
-print('-~'*35)
+print('-^'*35)
 print()
 for c in range(c, c + (10 - 1)* r, r):
     print(c, end=' → ')
