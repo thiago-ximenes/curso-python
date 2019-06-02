5193348be2cf1fdcdd42631dd00bff73d129421e
Thiago <thiagoximeneslima@gmail.com>
2019-06-02 14:37:39 -0300

    aula 16 e ex72

---
+++ pythonexercicios/ex072.py
@@ -0,0 +1,3 @@
+extenso = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'desesseis', 'desessete', 'desoito', 'desenove', 'vinte'
+n = int(input('Digite um número para mostra-lo por extenso: '))
+print(extenso[n])
---
+++ pythonteste/aula16.py
@@ -0,0 +1,23 @@
+#lanche = 'Hambúrguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita'
+#Tuplas são imutáveis!
+
+#for c in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[c]} na posição {c}')
+
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} e na posição {pos}')
+#print(sorted(lanche))
+
+#print('Já acabou?')
+
+#a = 2, 5, 4
+#b = 5, 8, 1, 2
+#c = b + a
+#print(c)
+#print(c.index(2, 4)) #depois da , é a partir daquela posição
+pessoa = 'Thiago', 27, 'M', 90.00
+del(pessoa) #deleta qualquer coisa no python #deleta tupla inteiro, não posso deletar só 1 item
+print(pessoa)
