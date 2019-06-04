8fe5c481afc7de7ebe3aaee57b3f01ba0aae3bdb
Thiago <thiagoximeneslima@gmail.com>
2019-06-03 23:51:18 -0300

    ex75 terminado

--- pythonexercicios/ex075.py
+++ pythonexercicios/ex075.py
@@ -2,15 +2,14 @@ tupla = (int(input('Digite um número: ')),
          int(input('Digite outro número: ')),
          int(input('Digite mais outro número: ')),
          int(input('Digite o último número: ')))
-nove = par = 0
+par = 0
 print(f'Você digitou os valores {tupla}')
-for pos, c in enumerate(tupla):
-    if c is 9:
-        nove += 1
-    if c is 3:
-        três = str(f'O valor 3 aparece na posição {pos}')
-    else:
-        três = str('O valor 3 não está em nenhuma posição.')
+print(f'O valor 9 aparece {tupla.count(9)} vezes.')
+print(f'O primeiro valor 3 está na posição {tupla.index(3)+1}' if 3 in tupla else 'O valor 3 aparece nenhuma vez.')
+print('Os números pares foram ', end='')
+for c in tupla:
     if c % 2 == 0:
-        par += 1
+        print(c, end=' ')
+print('\n')
         
+
