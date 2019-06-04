d8af314c605c5dc448a5b66acb73cfff0d73ac2c
Thiago <thiagoximeneslima@gmail.com>
2019-06-04 15:18:58 -0300

    corrigindo espaçamento

--- pythonexercicios/ex077.py
+++ pythonexercicios/ex077.py
@@ -3,4 +3,5 @@ for p in palavras:
     print(f' Na palavra {p} temos ', end='')
     for letra in p:
         if letra.lower() in 'aeiou':
-            print(letra, end='')
+            print(letra, end=' ')
+    print()
