1237390c2ec11c114649145a951514754d9bb2c3
Thiago <thiagoximeneslima@gmail.com>
2019-06-03 23:06:59 -0300

    teste

--- pythonexercicios/ex075.py
+++ pythonexercicios/ex075.py
@@ -2,9 +2,15 @@ tupla = (int(input('Digite um número: ')),
          int(input('Digite outro número: ')),
          int(input('Digite mais outro número: ')),
          int(input('Digite o último número: ')))
-nove = 0
-print(tupla)
-for c in tupla:
+nove = par = 0
+print(f'Você digitou os valores {tupla}')
+for pos, c in enumerate(tupla):
     if c is 9:
         nove += 1
-    if 
+    if c is 3:
+        três = str(f'O valor 3 aparece na posição {pos}')
+    else:
+        três = str('O valor 3 não está em nenhuma posição.')
+    if c % 2 == 0:
+        par += 1
+        
