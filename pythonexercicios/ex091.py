cf50ca3ded540c2b4e03b54b5ab4a46556e7e315
Thiago <thiagoximeneslima@gmail.com>
2019-06-12 11:45:51 -0300

    91

--- pythonexercicios/ex091.py
+++ pythonexercicios/ex091.py
@@ -1,19 +1,19 @@
 from random import randint
+#from operator import itemgetter
+from time import sleep
 
-game = []
 jogo = {}
-maior = []
 for j in range(0, 4):
-    jogo[f'jogador{j + 1}'] = randint(1, 6)
-    game.append(jogo.copy())
-    jogo.clear()
+	jogo[f'jogador{j + 1}'] = randint(1, 6)
 print('Valores sorteados:')
-for c in game:
-    for k, v in c.items():
-        print(f'{k} tirou {v} no dado.')
+for k, v in jogo.items():
+	print(f'{k} tirou {v} no dado.')
+	sleep(1)
 print('-=' * 20)
-maior.append(game[0])
-for c in game:
-    for v in c.values():
-        if 
-        
+#rank = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('RANKING')
+for i, j in enumerate(sorted(jogo, key=jogo.get, reverse=True)):
+	print(f'Na {i + 1}ª posição o {j} tirou número {jogo[j]} no dado')
+	sleep(1)
+#for i, c in enumerate(rank):
+#	print(f'Na {i + 1}ª posição o {c[0]} tirou o número {c[1]} no dado')
