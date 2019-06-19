a707af949150e55bb321f7da178d67dbc0771387
Thiago <thiagoximeneslima@gmail.com>
2019-06-19 16:53:16 -0300

    ex resolvidos

--- pythonexercicios/ex098.py
+++ pythonexercicios/ex098.py
@@ -1,6 +1,7 @@
 from time import sleep
 
-títu
+def título():
+    print('-' * 30)
 
 def contador(inicio, fim, passo):   
     print('-=' * 30)
@@ -9,13 +10,23 @@ def contador(inicio, fim, passo):
     print(f'Contagem de {inicio} até {fim} de {passo} em {passo}:')
     if inicio > fim:
         passo *= -1
-    while c in range(inicio, fim + 1, passo):
-        print(c, end=' ')
-    print()
+        for c in range(inicio, fim-1 , passo):
+            print(f'{c} ', end='', flush=True)
+            sleep(0.5)
+        print()
+    else:
+        for c in range(inicio, fim+1, passo):
+            print(f'{c} ', end='', flush=True)
+            sleep(0.5)
+        print()
+        
+    
 
 
 contador(1, 10, 1)
+título()
 contador(10, 0, 2)
+título()
 inicio = int(input('Digite o ínicio: '))
 fim = int(input('Digite o fim: '))
 passo = int(input('Digite o intervalo: '))
---
+++ pythonexercicios/ex099.py
@@ -0,0 +1,25 @@
+from time import sleep
+
+
+'''def maior(*lista):
+    print(len(lista))
+    print(max(lista)) '''
+
+
+def maior(*lista):
+    print('-' * 30)
+    print('Analisando os valores passados...')
+    for c in lista:
+        print(c, end=' ', flush=True)
+        sleep(0.5)
+    m = 0
+    for c in lista:
+        if c > m:
+            m = c
+    print(f'Foram passados {len(lista)} parâmetros')
+    print(f'O maior número passado por parâmetro foi o {m}')
+
+
+maior(19, 2, 1)
+maior(15, 24, 31, 2, 7)
+maior()
